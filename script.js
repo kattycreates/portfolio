6cccbb2a956677a0c6d68a04d3de5a9936ab0729
kattycreates <iamkarthika7@gmail.com>
2021-12-27 13:29:42 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggle(){
+    let value=document.getElementsByClassName("vertical-menubar")[0].style.display;
+    if(value==="none")
+    document.getElementsByClassName("vertical-menubar")[0].style.display="block";
+    else document.getElementsByClassName("vertical-menubar")[0].style.display="none";
+}
+/*
+function hover(){
+    document.getElementsByClassName('animations')[0].style.backgroundImage="url('./logo1.png')";
+
+}
+function bg(){
+    document.getElementsByClassName('animations')[0].style.backgroundImage="url('./avatar.png')";
+
+}
+*/
